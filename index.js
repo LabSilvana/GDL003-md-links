52521c6628ff5d985a53e863ab5273421e653d3c
Silvana <silvana15_91@hotmail.com>
2019-08-18 14:43:00 -0500

    Se agrego funciones

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-module.exports = () => {
-  // ...
+const path = require('path');
+
+const mdLinks = (filePath, options = { validate: false }) => {
+  if(checkAbsolute) {
+    if(checkMdFile(filePath)) {
+      console.log(__dirname);
+    }
+  }else {
+    if (checkMdFile(__dirname + filePath)) {
+      console.log(__dirname);
+    }
+  }
 };
+
+const checkAbsolute = (filePath) => {
+  return path.isAbsolute(filePath);
+}
+
+const checkMdFile = (filePath) => {
+  if (path.extname(filePath) === '.md') {
+    return true;
+  }   
+  return false;
+}
+
+
+module.exports = {mdLinks, checkAbsolute, checkMdFile};
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,17 @@
-const mdLinks = require('../');
-
+const {mdLinks, checkAbsolute, checkMdFile} = require('../');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should return true for a valid .md file', () => {
+    expect(checkMdFile('../README.md')).toBe(true);
   });
 
+  it('should return false for an invalid file', () => {
+    expect(checkMdFile('../index.js')).toBe(false);
+  });
+
+  it('should check if path is absolute', () => {
+    expect(checkAbsolute('./index.js')).toBe(false);
+    expect(checkAbsolute('/index.js')).toBe(true);
+  });
 });
